2b0ab205c4d17f2d8f8e5872f133081ff6162dc4
jaehyeok-jeong <jaehyeok.jeong@nhn.com>
2023-11-12 23:32:07 +0900

    4.3.3 필드 접근 방식
    - Member에 예제 추가

--- src/main/java/com/nhn/cloud/ddd/member/domain/Member.java
+++ src/main/java/com/nhn/cloud/ddd/member/domain/Member.java
@@ -1,10 +1,13 @@
 package com.nhn.cloud.ddd.member.domain;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import com.nhn.cloud.ddd.common.domain.EmailSet;
 import com.nhn.cloud.ddd.common.jpa.EmailSetConverter;
@@ -15,7 +18,19 @@ public class Member {
     @EmbeddedId
     private MemberId memberId;
 
+    @Transient
+    private String firstName;
+    @Transient
+    private String lastName;
+
+    private String memberName;
+
     @Column(name = "emails")
     @Convert(converter = EmailSetConverter.class)
     private EmailSet emails;
+
+    @Access(AccessType.PROPERTY)
+    public String getMemberName() {
+        return firstName + lastName;
+    }
 }
