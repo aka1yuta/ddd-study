1c5c09745750e85e7f1307545d12de6a7f9e6e46
jaehyeok-jeong <jaehyeok.jeong@nhn.com>
2023-11-12 18:43:53 +0900

    4.3.2 기본 생성자

--- src/main/java/com/nhn/cloud/ddd/order/domain/Receiver.java
+++ src/main/java/com/nhn/cloud/ddd/order/domain/Receiver.java
@@ -3,10 +3,15 @@ package com.nhn.cloud.ddd.order.domain;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.AllArgsConstructor;
+
 @Embeddable
+@AllArgsConstructor
 public class Receiver {
     @Column(name = "receiver_name")
     private String name;
     @Column(name = "receiver_phone")
     private String phone;
+
+    protected Receiver() { }
 }
